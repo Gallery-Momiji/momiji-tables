c8211a0b475058386ff27f296cf8aad4ecbfbbd0
Mystro256 <alexjnewt@hotmail.com>
2016-02-09 23:12:42 -0500

    Commenting most of the fields in table.sql

--- tables.sql
+++ tables.sql
@@ -6,63 +6,63 @@ USE `gallery`;
 -- Dumping structure for table gallery.artists
 DROP TABLE IF EXISTS `artists`;
 CREATE TABLE IF NOT EXISTS `artists` (
-  `ArtistID` int(11) NOT NULL,
-  `ArtistName` tinytext,
-  `ArtistEmail` tinytext,
-  `ArtistAddress` text,
-  `ArtistUrl` tinytext,
-  `ArtistAgentName` tinytext,
-  `ArtistAgentPhone` tinytext,
-  `ArtistAPP` tinyint(1) NOT NULL DEFAULT '0',
-  `ArtistPhone` tinytext,
-  `ArtistAgentAddress` tinytext,
-  `ArtistAgentEmail` tinytext,
-  `ArtistShowName` tinytext,
-  `ArtistTables` float DEFAULT '0',
-  `ArtistPanels` float DEFAULT '0',
+  `ArtistID` int(11) NOT NULL COMMENT 'Unique Artist ID',
+  `ArtistName` tinytext COMMENT 'Artist Name',
+  `ArtistEmail` tinytext COMMENT 'Artist Email',
+  `ArtistAddress` text COMMENT 'Artist Mailing Address',
+  `ArtistUrl` tinytext COMMENT 'Artist webpage or site',
+  `ArtistAgentName` tinytext COMMENT 'Artist Agent Name',
+  `ArtistAgentPhone` tinytext COMMENT 'Artist Agent Phone No',
+  `ArtistAPP` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'PS Accredited Press Photography',
+  `ArtistPhone` tinytext COMMENT 'Artist Phone No',
+  `ArtistAgentAddress` tinytext COMMENT 'Artist Agent Address',
+  `ArtistAgentEmail` tinytext COMMENT 'Artist Agent Email',
+  `ArtistShowName` tinytext COMMENT 'Artist Show/Nickname',
+  `ArtistTables` float DEFAULT '0' COMMENT 'Artist Table Num',
+  `ArtistPanels` float DEFAULT '0' COMMENT 'Artist Panel Num',
   `ArtistDue` float DEFAULT '0',
   `ArtistPaid` float DEFAULT '0',
   `ArtistPaidFully` tinyint(1) DEFAULT '0',
-  `ArtistCheckIn` tinyint(1) DEFAULT '0',
-  `ArtistcheckOut` tinyint(1) DEFAULT '0',
+  `ArtistCheckIn` tinyint(1) DEFAULT '0' COMMENT 'Artist is checked in (YES/NO)',
+  `ArtistcheckOut` tinyint(1) DEFAULT '0' COMMENT 'Artist is checked out (YES/NO)',
   PRIMARY KEY (`ArtistID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Dumping structure for table gallery.gsmerchandise
 DROP TABLE IF EXISTS `gsmerchandise`;
 CREATE TABLE IF NOT EXISTS `gsmerchandise` (
-  `ArtistID` tinyint(3) unsigned NOT NULL,
-  `PieceID` tinyint(3) unsigned NOT NULL,
-  `PieceTitle` tinytext NOT NULL,
-  `PiecePrice` float NOT NULL,
-  `PieceInitialStock` smallint(6) NOT NULL DEFAULT '0' COMMENT 'How many we got to sell ',
-  `PieceStock` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'How many of this item there is',
-  `PieceSDC` tinyint(1) DEFAULT '1'
+  `ArtistID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Artist ID',
+  `PieceID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Piece ID',
+  `PieceTitle` tinytext NOT NULL COMMENT 'Piece Title/Name',
+  `PiecePrice` float NOT NULL COMMENT 'Piece Price',
+  `PieceInitialStock` smallint(6) NOT NULL DEFAULT '0' COMMENT 'Number to sell',
+  `PieceStock` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Number left in stock',
+  `PieceSDC` tinyint(1) DEFAULT '1' COMMENT 'Sell display copy (YES/NO)'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- Dumping structure for table gallery.log
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `action` mediumtext NOT NULL,
-  `timestamp` time NOT NULL,
-  `date` date NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Log ID',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Associated username',
+  `action` mediumtext NOT NULL COMMENT 'Action logged',
+  `timestamp` time NOT NULL COMMENT 'Timestamp of log',
+  `date` date NOT NULL COMMENT 'Date of log',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=5235 DEFAULT CHARSET=latin1;
 
 -- Dumping structure for table gallery.merchandise
 DROP TABLE IF EXISTS `merchandise`;
 CREATE TABLE IF NOT EXISTS `merchandise` (
-  `ArtistID` int(10) unsigned NOT NULL,
-  `MerchID` int(10) unsigned NOT NULL,
-  `MerchTitle` tinytext NOT NULL,
-  `MerchMinBid` float unsigned NOT NULL,
+  `ArtistID` int(10) unsigned NOT NULL COMMENT 'Unique Artist ID',
+  `MerchID` int(10) unsigned NOT NULL COMMENT 'Unique Merch ID',
+  `MerchTitle` tinytext NOT NULL COMMENT 'Merch Title/Name',
+  `MerchMinBid` float unsigned NOT NULL COMMENT 'Merch minimum Bid',
   `MerchAAMB` tinyint(1) unsigned NOT NULL,
-  `MerchQuickSale` float unsigned NOT NULL,
-  `MerchMedium` tinytext NOT NULL,
-  `MerchSold` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `ReceiptID` int(11) DEFAULT NULL,
+  `MerchQuickSale` float unsigned NOT NULL COMMENT 'Merch quicksale price',
+  `MerchMedium` tinytext NOT NULL COMMENT 'Merch medium description',
+  `MerchSold` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Merch sold (YES/NO)',
+  `ReceiptID` int(11) DEFAULT NULL COMMENT 'Unique Receipt Id if sold',
   KEY `Index 1` (`ArtistID`,`MerchID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -78,14 +78,14 @@ CREATE TABLE IF NOT EXISTS `options` (
 -- Dumping structure for table gallery.receipts
 DROP TABLE IF EXISTS `receipts`;
 CREATE TABLE IF NOT EXISTS `receipts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL DEFAULT '0',
-  `price` float NOT NULL DEFAULT '0',
-  `paid` float NOT NULL DEFAULT '0',
-  `isQuickSale` tinyint(1) NOT NULL DEFAULT '0',
-  `isAuctionSale` tinyint(1) NOT NULL DEFAULT '0',
-  `isGalleryStoreSale` tinyint(1) NOT NULL DEFAULT '0',
-  `isPrinted` tinyint(1) DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Receipt Id',
+  `userID` int(11) NOT NULL DEFAULT '0' COMMENT 'Associated username',
+  `price` float NOT NULL DEFAULT '0' COMMENT 'Price sold for',
+  `paid` float NOT NULL DEFAULT '0' COMMENT 'Amount paid',
+  `isQuickSale` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Quicksale (YES/NO)',
+  `isAuctionSale` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Auction sale (YES/NO)',
+  `isGalleryStoreSale` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Gallery store sale (YES/NO)',
+  `isPrinted` tinyint(1) DEFAULT '0' COMMENT 'Printed (YES/NO)',
   `itemArray` mediumtext,
   `priceArray` mediumtext,
   PRIMARY KEY (`id`)
@@ -95,11 +95,11 @@ CREATE TABLE IF NOT EXISTS `receipts` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'ID number for each user',
-  `username` tinytext,
-  `password` tinytext,
-  `class` tinyint(4) DEFAULT NULL,
-  `name` tinytext,
-  `banner` varchar(50) DEFAULT 'newbie',
+  `username` tinytext COMMENT 'username of user',
+  `password` tinytext COMMENT 'md5sum of user password',
+  `class` tinyint(4) DEFAULT NULL COMMENT 'Permission level of user',
+  `name` tinytext COMMENT 'Name of user',
+  `banner` varchar(50) DEFAULT 'newbie' COMMENT 'Deprecated, tentative deletion',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
