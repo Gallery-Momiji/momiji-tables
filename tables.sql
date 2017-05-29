a5a26cb874f9227de2365808ae1d149f5a5886fe
Mystro256 <alexjnewt@hotmail.com>
2017-05-29 16:47:22 -0400

    Better use of types, fix auto incrementers

--- tables.sql
+++ tables.sql
@@ -6,7 +6,7 @@ USE `gallery`;
 DROP TABLE IF EXISTS `artists`;
 CREATE TABLE IF NOT EXISTS `artists` (
 -- This table is used to store artist information
-  `ArtistID` int(11) NOT NULL COMMENT 'Unique Artist ID',
+  `ArtistID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Artist ID',
   `ArtistName` tinytext COMMENT 'Artist Name',
   `ArtistEmail` tinytext COMMENT 'Artist Email',
   `ArtistAddress` text COMMENT 'Artist Mailing Address',
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS `gsmerchandise` (
   `PieceID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Piece ID',
   `PieceTitle` tinytext NOT NULL COMMENT 'Piece Title/Name',
   `PiecePrice` float NOT NULL COMMENT 'Piece Price',
-  `PieceInitialStock` smallint(6) NOT NULL DEFAULT '0' COMMENT 'Number to sell',
-  `PieceStock` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'Number left in stock',
+  `PieceInitialStock` smallint unsigned NOT NULL DEFAULT '0' COMMENT 'Number to sell',
+  `PieceStock` smallint unsigned NOT NULL DEFAULT '0' COMMENT 'Number left in stock',
   `PieceSDC` tinyint(1) DEFAULT '1' COMMENT 'Sell display copy (YES/NO)',
   KEY `GSMerchKey` (`ArtistID`,`PieceID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -44,43 +44,43 @@ CREATE TABLE IF NOT EXISTS `gsmerchandise` (
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
 -- This table is used to store POS log information
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Log ID',
-  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'Associated username',
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Log ID',
+  `user_id` smallint unsigned NOT NULL DEFAULT '0' COMMENT 'Associated username',
   `action` mediumtext NOT NULL COMMENT 'Action logged',
   `timestamp` time NOT NULL COMMENT 'Timestamp of log',
   `date` date NOT NULL COMMENT 'Date of log',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5235 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `merchandise`;
 CREATE TABLE IF NOT EXISTS `merchandise` (
 -- This table is used to store auction merchandise information
-  `ArtistID` int(11) unsigned NOT NULL COMMENT 'Unique Artist ID',
-  `MerchID` int(11) unsigned NOT NULL COMMENT 'Unique Merch ID',
+  `ArtistID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Artist ID',
+  `MerchID` tinyint(3) unsigned NOT NULL COMMENT 'Unique Merch ID',
   `MerchTitle` tinytext NOT NULL COMMENT 'Merch Title/Name',
   `MerchMinBid` float unsigned NOT NULL COMMENT 'Merch minimum Bid',
   `MerchAAMB` tinyint(1) unsigned NOT NULL COMMENT 'After Auction Minimum Bid (YES/NO)',
   `MerchQuickSale` float unsigned NOT NULL COMMENT 'Merch quicksale price',
   `MerchMedium` tinytext NOT NULL COMMENT 'Merch medium description',
   `MerchSold` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Merch sold (YES/NO)',
-  `ReceiptID` int(11) DEFAULT NULL COMMENT 'Unique Receipt Id if sold',
+  `ReceiptID` smallint unsigned DEFAULT NULL COMMENT 'Unique Receipt Id if sold',
   KEY `MerchKey` (`ArtistID`,`MerchID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `options`;
 CREATE TABLE IF NOT EXISTS `options` (
 -- This table is used to store various information, such as panel prices and special dates
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique ID',
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique ID',
   `option` tinytext NOT NULL COMMENT 'Option Name',
   `value` tinytext NOT NULL COMMENT 'Option Value',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `receipts`;
 CREATE TABLE IF NOT EXISTS `receipts` (
 -- This table is used to store receipt information
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Receipt Id',
-  `userID` int(11) NOT NULL DEFAULT '0' COMMENT 'Associated username',
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Receipt Id',
+  `userID` smallint unsigned NOT NULL DEFAULT '0' COMMENT 'Associated username',
   `price` float NOT NULL DEFAULT '0' COMMENT 'Price sold for',
   `paid` float NOT NULL DEFAULT '0' COMMENT 'Amount paid',
   `isQuickSale` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Quicksale (YES/NO)',
@@ -98,22 +98,22 @@ CREATE TABLE IF NOT EXISTS `receipts` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
 -- This table is used to store user information
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID number for each user',
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID number for each user',
   `username` tinytext COMMENT 'username of user',
   `password` tinytext COMMENT 'md5sum of user password',
-  `class` tinyint(4) DEFAULT NULL COMMENT 'Permission level of user',
+  `class` tinyint(3) unsigned DEFAULT NULL COMMENT 'Permission level of user',
   `name` tinytext COMMENT 'Name of user',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `bidders`;
 CREATE TABLE IF NOT EXISTS `bidders` (
 -- This table is used to store bidder information
   `name` tinytext NOT NULL COMMENT 'Name of Bidder',
-  `bidderno` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Bidder No',
+  `bidderno` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Bidder No',
   `phoneno` tinytext NOT NULL COMMENT 'Bidder Phone Number',
   `eaddress` tinytext NOT NULL COMMENT 'Email Address',
   `maddress` text NOT NULL COMMENT 'Mailing address',
   `due` float DEFAULT '0' COMMENT 'Money still owed by bidder',
   PRIMARY KEY (`bidderno`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
